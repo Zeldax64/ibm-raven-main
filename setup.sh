cfefb2a89c8afe07f2dea0a7c81169c1d03bdf29
Caio Vieira <cravieira@inf.ufrgs.br>
2024-05-28 22:18:54 -0300

    Add setup.sh
    Add a script to initialize the repository requirements and download
    RAVEN dataset.

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -eu
+
+DATASET_DIR='_data'
+RAVEN_DATASET="$DATASET_DIR/raven"
+
+RAVEN_REPO='dataset/raven'
+
+function download_raven() {
+    local wd=$PWD
+    local dataset=RAVEN-10000-release.zip
+
+    mkdir -p $DATASET_DIR
+
+    source _venv/bin/activate
+    cd $DATASET_DIR
+    pip install gdown
+    # Original RAVEN-10000 dataset provided by the paper's authors
+    gdown 111swnEzAY2NfZgeyAhVwQujMjRUfeyuY -O "$dataset"
+    deactivate
+
+    echo "Unzipping dataset..."
+    unzip -q "$dataset"
+    rm "$dataset"
+
+    cd $wd
+}
+
+function setup_venv() {
+    python3 -m venv _venv
+    source _venv/bin/activate
+    pip install -e nvsa
+
+    # Install required packages
+    pip install gdown
+    deactivate
+}
+
+#setup_venv
+download_raven
